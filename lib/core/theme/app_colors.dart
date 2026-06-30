2a17b967871c0426689ea9274d15dd7490ead29d
Satriahrl_ <satriaherlambang22@gmail.com>
2026-07-01 03:54:17 +0700

    feat: mengganti warna utama menjadi gradasi warna ungu.

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // Primary Blue
-  static const Color primary = Color(0xFF0B63E5);
-  static const Color primaryLight = Color(0xFF2C8BFF);
-  static const Color primaryDark = Color(0xFF0A4FBF);
+  static const Color primary = Color(0xFF987BFE);
+  static const Color primaryLight = Color(0xFF7A5AF8);
+  static const Color primaryDark = Color(0xFF5A36EC);
   static const Color primarySurface = Color(0xFFE8F1FD);
   static const Color primaryBorder = Color(0xFFB8D0F7);
 
